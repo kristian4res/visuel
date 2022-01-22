fb82930a851fb169d1d3a9df6e7e7b8d17ba8f7a
Enigma-cloud <torreskristian77@gmail.com>
2022-01-22 22:45:48 +0000

    cs: planning ui and creating components folder

---
+++ src/components/button/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Navigation.scss';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <p>Sign Out</p>
+        </nav>
+    )
+};
+
+export default Navigation;
---
+++ src/components/button/button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const button = () => {
+  return <div>THIS IS A BUTTON</div>;
+};
+
+export default button;
